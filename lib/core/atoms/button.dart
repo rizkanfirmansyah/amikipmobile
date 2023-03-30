5a4676e6acd60830e5f690c53578d0e262afb0a9
Rizkan-Dev-Win <riezkanaprianda@gmail.com>
2023-03-30 10:54:27 +0700

    update

---
+++ lib/core/atoms/button.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('heheheheh');
+  }
+}
---
+++ lib/core/foundations/fonts.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+import 'package:amikipmobile/core/tokens/colors.dart';
+
+class Fonts {
+  static const Color bodyLarge = Tokens.primary;
+}
